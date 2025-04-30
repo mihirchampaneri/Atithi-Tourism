b0b58e08515ffba42f7570dbe113f2df1279a893
mihirchampaneri <mhrchampaneri@gmail.com>
2025-04-30 17:26:57 +0530

    Multer Size 10mb fix

--- config/multer-config.js
+++ config/multer-config.js
@@ -1,6 +1,8 @@
 const multer= require('multer');
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage, limits: {
+    fieldSize: 10 * 1024 * 1024, // 10 MB field size limit for text fields
+    fileSize: 5 * 1024 * 1024}, });
 
 module.exports = upload;
